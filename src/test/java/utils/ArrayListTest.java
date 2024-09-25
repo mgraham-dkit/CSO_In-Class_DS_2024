e617b78d4eb9230cad29600cfd93df710f1fbc1a
michelle <michelle.graham@dkit.ie>
2024-09-25 21:34:28 +0100

    Testing: Added test cases for add(): - Adding to populated list. - Adding to populated list beyond initial capacity.

--- src/test/java/utils/ArrayListTest.java
+++ src/test/java/utils/ArrayListTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayListTest {
 
     @Test
-    void add() {
+    void add_EmptyList() {
         ArrayList myList = new ArrayList();
         int value = 5;
         myList.add(value);
@@ -16,5 +16,42 @@ class ArrayListTest {
 
         int retrieved = myList.get(0);
         assertEquals(value, retrieved);
+    }
+
+    @Test
+    void add_PopulatedList() {
+        ArrayList myList = new ArrayList();
+        int [] values = {1, 5, 7, 9};
+        for (int value : values) {
+            myList.add(value);
+        }
+
+        assertEquals(values.length, myList.size());
+
+        for (int i = 0; i < myList.size(); i++) {
+            int retrieved = myList.get(i);
+            assertEquals(values[i], retrieved);
+        }
+    }
+
+    @Test
+    void add_PopulatedList_GrowRequired() {
+        ArrayList myList = new ArrayList();
+
+        int [] values = new int[11];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = i;
+        }
+
+        for (int value : values) {
+            myList.add(value);
+        }
+
+        assertEquals(values.length, myList.size());
+
+        for (int i = 0; i < myList.size(); i++) {
+            int retrieved = myList.get(i);
+            assertEquals(values[i], retrieved);
+        }
     }
 }
